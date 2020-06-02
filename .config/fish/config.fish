d63a388d0cacb61656fed296e9edc79830523302
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2020-06-02 22:41:10 +0100

    Add pyenv(-virtualenv)
    Add environment variables and `pyenv init` to shell configs.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,13 @@ set fish_greeting
 set -gx EDITOR nvim
 set -gx LANG en_US.UTF-8
 
+
 set -gx PATH ~/bin /usr/local/bin /usr/bin /usr/local/sbin
 set -gx PATH $PATH usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
+
+set -gx PIP_REQUIRE_VIRTUALENV true
+set -gx PYENV_ROOT $HOME/.pyenv
+set -gx PATH $PYENV_ROOT/bin $PATH
+
+pyenv init - | source
+pyenv virtualenv-init - | source
--- .profile
+++ .profile
@@ -4,3 +4,9 @@ export PATH=~/bin:$PATH
 
 export EDITOR=nvim
 export LANG=en_US.UTF-8
+
+export PIP_REQUIRE_VIRTUALENV=true
+
+export PYENV_ROOT=$HOME/.pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
+eval "$(pyenv init -)"
